d65775e2b018b8f31780a91ffcefad79bffe84e5
leedavidcs <lee.david.cs@gmail.com>
2020-07-10 04:13:59 -0700

    Updates

---
+++ precache-manifest.99be007bfde4ae7b567890ac3af0a13a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3234c37b831d9d22d4c08ce8d8c12a6f",
+    "url": "/leedavidcs/index.html"
+  },
+  {
+    "revision": "da0f6a7d4687c8d78d46",
+    "url": "/leedavidcs/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "312b068ba4fa5103e28a",
+    "url": "/leedavidcs/static/js/2.3d428f08.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/leedavidcs/static/js/2.3d428f08.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da0f6a7d4687c8d78d46",
+    "url": "/leedavidcs/static/js/main.33b76aa5.chunk.js"
+  },
+  {
+    "revision": "0f59e25ec58e2c0d521d",
+    "url": "/leedavidcs/static/js/runtime-main.b36abcac.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/leedavidcs/static/media/logo.5d5d9eef.svg"
+  }
+]);
