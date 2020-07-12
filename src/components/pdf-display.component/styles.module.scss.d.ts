6b0dc810cba294910fc0262511351519cbc4bffc
leedavidcs <lee.david.cs@gmail.com>
2020-07-12 09:30:16 -0700

    Update letter-page to be without box-shadow. Add pdf display

---
+++ src/components/pdf-display.component/styles.module.scss.d.ts
@@ -0,0 +1,3 @@
+export const controlContainer: string;
+export const downloadBtn: string;
+export const sourceContainer: string;
---
+++ src/page-parts/resume.page-part.tsx
@@ -0,0 +1,16 @@
+import { Container, Section, SubSection } from "@/components";
+import React, { FC } from "react";
+
+export const Resume: FC = () => {
+	return (
+		<div>
+			<Container>
+				<Container.Column>
+					<Section title="Experience">
+						<SubSection title="Full Stack Engineer">asdf</SubSection>
+					</Section>
+				</Container.Column>
+			</Container>
+		</div>
+	);
+};
