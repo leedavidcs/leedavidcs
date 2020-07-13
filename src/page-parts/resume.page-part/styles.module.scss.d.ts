7361fcfdd5765339e7edbccc5521e0764972f28c
leedavidcs <lee.david.cs@gmail.com>
2020-07-13 12:11:41 -0700

    Completed v0 resume

--- src/page-parts/resume.page-part/styles.module.scss.d.ts
+++ src/page-parts/resume.page-part/styles.module.scss.d.ts
@@ -4,6 +4,8 @@ export const name: string;
 export const occupation: string;
 export const iconText: string;
 export const contactUrls: string;
+export const jobType: string;
 export const info: string;
 export const qualifications: string;
 export const skills: string;
+export const skillsSection: string;
