73334d0239ba11b4c5e7d2fd8a8c78dd3f0f3bb9
leedavidcs <lee.david.cs@gmail.com>
2020-07-16 12:36:43 -0700

    Rearrange contacts

--- src/page-parts/resume.page-part/styles.module.scss.d.ts
+++ src/page-parts/resume.page-part/styles.module.scss.d.ts
@@ -3,7 +3,6 @@ export const header: string;
 export const name: string;
 export const occupation: string;
 export const contact: string;
-export const iconText: string;
 export const jobType: string;
 export const qualifications: string;
 export const skills: string;
