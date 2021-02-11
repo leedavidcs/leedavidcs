eca72a283806ddc6e2e2db97376a89aef64b96c9
leedavidcs <lee.david.cs@gmail.com>
2021-02-11 14:25:20 -0800

    Updated styles. Split off colors to separate file, regenerate typings

---
+++ src/colors.ts
@@ -0,0 +1,7 @@
+export const background = "#fff";
+export const onBackground = "#000";
+export const brand = "#663399";
+export const onBrand = "#fff";
+export const secondary = "#00ffff";
+export const onSecondary = "#fff";
+export const hoverSecondary = "#0077ad";
