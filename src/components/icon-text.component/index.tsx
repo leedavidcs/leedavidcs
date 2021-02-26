add308c6b38f53322c2196d24d9f049835894e28
leedavidcs <lee.david.cs@gmail.com>
2021-02-26 04:28:08 -0800

    Fix icon color

--- src/components/icon-text.component/index.tsx
+++ src/components/icon-text.component/index.tsx
@@ -1,5 +1,5 @@
 import clsx from "clsx";
-import React, { FC, ReactNode } from "react";
+import React, { CSSProperties, FC, ReactNode } from "react";
 import type { IconType } from "react-icons";
 import classes from "./styles.module.scss";
 
@@ -7,17 +7,18 @@ interface IProps {
 	children?: ReactNode;
 	className?: string;
 	icon?: IconType;
+	style?: CSSProperties;
 	text?: ReactNode;
 }
 
-export const IconText: FC<IProps> = ({ children, className, icon, text }) => {
+export const IconText: FC<IProps> = ({ children, className, icon, style, text }) => {
 	return (
-		<div className={clsx(classes.root, className)}>
+		<div className={clsx(classes.root, className)} style={style}>
 			{icon &&
 				React.createElement(icon, {
 					className: classes.icon,
 					size: 12,
-					style: { fill: "#fff" }
+					style: { fill: "currentColor", marginRight: 4 }
 				})}
 			{text || children}
 		</div>
